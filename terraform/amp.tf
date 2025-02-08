f8b30c4c128ecdddba4e9f72e6599924e175f930
Michael Cramer <mjcramer@gmail.com>
2025-02-07 16:15:02 -0800

    add stuff

--- terraform/amp.tf
+++ terraform/amp.tf
@@ -1,15 +1,8 @@
 ## Create prometheus workspace, log group and alert manager. Change to name to what you want to name it.
 
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/prometheus_workspace
 resource "aws_prometheus_workspace" "amp_workspace" {
-   alias = "aep1-stage-va6-amp-poc"
-   
-   tags = {
-       "Adobe.Environment" = "stage"
-       "Adobe.Location"    = "va6"
-       "Adobe.Region"      = "us-east-1"
-       "Adobe.ArchPath"    = ""
-       "Adobe.Component"   = "" 
-   }
+   alias = "cramer-1"
 
    logging_configuration {
        log_group_arn = "${aws_cloudwatch_log_group.prometheus_logs.arn}:*"
@@ -20,14 +13,14 @@ resource "aws_cloudwatch_log_group" "prometheus_logs" {
    name = "aws-managed-service-prometheus-complete"
 }
 
-resource "aws_prometheus_alert_manager_definition" "alerts" {
-   workspace_id = aws_prometheus_workspace.amp_workspace.id
-   
-   definition = <<-EOT
-       alertmanager_config: |
-         route:
-           receiver: 'default'
-         receivers:
-           - name: 'default'
-   EOT
-   }
+# resource "aws_prometheus_alert_manager_definition" "alerts" {
+#    workspace_id = aws_prometheus_workspace.amp_workspace.id
+#
+#    definition = <<-EOT
+# alertmanager_config: |
+#   route:
+#     receiver: 'default'
+#     receivers:
+#       - name: 'default'
+# EOT
+# }
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,76 +1,83 @@
+
 provider "aws" {
-  region = "us-east-1"  # Change to your desired AWS region
+  region = "us-west-2"  # Change to your desired AWS region
+  profile = "default"
 }
 
-# 🔹 S3 Bucket for Firehose
-resource "aws_s3_bucket" "firehose_bucket" {
-  bucket = "my-kinesis-firehose-bucket"
+variable "region" {
+  default = "us-west-2"
 }
 
-# 🔹 IAM Role for Firehose
-resource "aws_iam_role" "firehose_role" {
-  name = "firehose_delivery_role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Service = "firehose.amazonaws.com"
-        }
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
+variable "account_id" {
+  default = "240259995564"
 }
 
-# 🔹 IAM Policy for Firehose
-resource "aws_iam_policy" "firehose_policy" {
-  name        = "firehose_s3_policy"
-  description = "Allows Firehose to write to S3"
-  
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "s3:PutObject",
-          "s3:GetBucketLocation",
-          "s3:ListBucket"
-        ]
-        Resource = [
-          aws_s3_bucket.firehose_bucket.arn,
-          "${aws_s3_bucket.firehose_bucket.arn}/*"
-        ]
-      }
-    ]
-  })
-}
 
-# 🔹 Attach IAM Policy to Role
-resource "aws_iam_role_policy_attachment" "firehose_policy_attach" {
-  role       = aws_iam_role.firehose_role.name
-  policy_arn = aws_iam_policy.firehose_policy.arn
-}
 
-# 🔹 Kinesis Firehose Delivery Stream
-resource "aws_kinesis_firehose_delivery_stream" "firehose" {
-  name        = "my-firehose-stream"
-  destination = "s3"
+# # 🔹 S3 Bucket for Firehose
+# resource "aws_s3_bucket" "firehose_bucket" {
+#   bucket = "my-kinesis-firehose-bucket"
+# }
+#
+# # 🔹 IAM Role for Firehose
+# resource "aws_iam_role" "firehose_role" {
+#   name = "firehose_delivery_role"
+#
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect = "Allow"
+#         Principal = {
+#           Service = "firehose.amazonaws.com"
+#         }
+#         Action = "sts:AssumeRole"
+#       }
+#     ]
+#   })
+# }
+#
+# # 🔹 IAM Policy for Firehose
+# resource "aws_iam_policy" "firehose_policy" {
+#   name        = "firehose_s3_policy"
+#   description = "Allows Firehose to write to S3"
+#
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect = "Allow"
+#         Action = [
+#           "s3:PutObject",
+#           "s3:GetBucketLocation",
+#           "s3:ListBucket"
+#         ]
+#         Resource = [
+#           aws_s3_bucket.firehose_bucket.arn,
+#           "${aws_s3_bucket.firehose_bucket.arn}/*"
+#         ]
+#       }
+#     ]
+#   })
+# }
 
-  s3_configuration {
-    role_arn   = aws_iam_role.firehose_role.arn
-    bucket_arn = aws_s3_bucket.firehose_bucket.arn
-    buffer_size = 5
-    buffer_interval = 300
-    compression_format = "GZIP"
-  }
-}
-
-# 🔹 Amazon Managed Prometheus (AMP) Workspace
-resource "aws_prometheus_workspace" "amp_workspace" {
-  alias = "my-amp-workspace"
-}
-
+# # 🔹 Attach IAM Policy to Role
+# resource "aws_iam_role_policy_attachment" "firehose_policy_attach" {
+#   role       = aws_iam_role.firehose_role.name
+#   policy_arn = aws_iam_policy.firehose_policy.arn
+# }
+#
+# # 🔹 Kinesis Firehose Delivery Stream
+# resource "aws_kinesis_firehose_delivery_stream" "firehose" {
+#   name        = "my-firehose-stream"
+#   destination = "s3"
+#
+#   s3_configuration {
+#     role_arn   = aws_iam_role.firehose_role.arn
+#     bucket_arn = aws_s3_bucket.firehose_bucket.arn
+#     buffer_size = 5
+#     buffer_interval = 300
+#     compression_format = "GZIP"
+#   }
+# }
+#
