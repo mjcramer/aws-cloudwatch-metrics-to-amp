e1166f57db9759ad39ecfbe9fa47f00fc5908661
Michael Cramer <mjcramer@gmail.com>
2025-02-07 16:24:58 -0800

    add kinesis

--- terraform/kinesis.tf
+++ terraform/kinesis.tf
@@ -130,3 +130,92 @@ resource "aws_kinesis_stream" "test_stream" {
 #   function_name    = aws_lambda_function.kinesis_to_amp.arn
 #   starting_position = "LATEST"
 # }
+
+
+
+resource "aws_kinesis_firehose_delivery_stream" "metric_firehose" {
+    name        = var.firehose_name
+    destination = "extended_s3"
+
+    extended_s3_configuration {
+        role_arn           = var.firehose_role_arn
+        bucket_arn         = var.s3_bucket_arn
+        buffering_size     = var.buffering_size     # In MiB, between 1-128
+        buffering_interval = var.buffering_interval # In seconds, between 60-900
+        compression_format = var.compression_format # GZIP, Snappy, ZIP, or UNCOMPRESSED
+        
+        cloudwatch_logging_options {
+            enabled         = var.cloudwatch_logging_enabled
+            log_group_name  = var.log_group_name
+            log_stream_name = var.log_stream_name
+        }
+
+        processing_configuration {
+            enabled = var.processing_enabled
+
+            processors {
+                type = "Lambda"
+                
+                parameters {
+                    parameter_name  = "LambdaArn"
+                    parameter_value = var.lambda_arn
+                }
+
+                # Optional: Include retry duration if needed
+                # parameters {
+                #     parameter_name  = "RetryDuration"
+                #     parameter_value = "60" # seconds
+                # }
+            }
+        }
+
+        # S3 backup mode is optional, default is "Disabled"
+        s3_backup_mode = "Disabled"
+    }
+
+    server_side_encryption {
+        enabled  = var.sse_enabled
+        key_type = var.sse_key_type # can be "AWS_OWNED_CMK" or "CUSTOMER_MANAGED_CMK"
+    }
+
+    tags = var.tags
+}
+
+# Required variables
+variable "firehose_name" {
+    type        = string
+    description = "Name of the Kinesis Firehose delivery stream"
+}
+
+variable "s3_bucket_arn" {
+    type        = string
+    description = "ARN of the destination S3 bucket"
+}
+
+variable "firehose_role_arn" {
+    type        = string
+    description = "ARN of the IAM role that Kinesis Firehose can assume"
+}
+
+# Optional variables with defaults
+variable "buffering_interval" {
+    type        = number
+    default     = 300
+    description = "Buffer incoming data for the specified period of time, in seconds"
+
+    validation {
+        condition     = var.buffering_interval >= 60 && var.buffering_interval <= 900
+        error_message = "Buffering interval must be between 60 and 900 seconds."
+    }
+}
+
+variable "buffering_size" {
+    type        = number
+    default     = 5
+    description = "Buffer incoming data to the specified size, in MiBs"
+
+    validation {
+        condition     = var.buffering_size >= 1 && var.buffering_size <= 128
+        error_message = "Buffering size must be between 1 and 128 MiBs."
+    }
+}
