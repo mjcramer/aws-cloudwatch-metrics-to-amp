a38a6134554de2fe83028776a33d9a58f4aa916b
Michael Cramer <mjcramer@gmail.com>
2025-02-14 10:29:47 -0800

    change metric schema

--- terraform/firehose.tf
+++ terraform/firehose.tf
@@ -20,98 +20,71 @@ resource "aws_iam_role" "firehose_role" {
   assume_role_policy = data.aws_iam_policy_document.firehose_assume_role.json
 }
 
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_policy
-resource "aws_iam_policy" "firehose_policy" {
-  name        = "firehose_s3_policy"
-  description = "Allows Firehose to write to S3"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "s3:PutObject",
-          "s3:GetBucketLocation",
-          "s3:ListBucket"
-        ]
-        Resource = [
-          aws_s3_bucket.metrics_bucket.arn,
-          "${aws_s3_bucket.metrics_bucket.arn}/*"
-        ]
-      }
-    ]
-  })
-}
-
 # 🔹 Attach IAM Policy to Role
 resource "aws_iam_role_policy_attachment" "firehose_policy_attach" {
   role       = aws_iam_role.firehose_role.name
   policy_arn = aws_iam_policy.firehose_policy.arn
 }
 
-# resource "aws_iam_policy" "kinesis_access_policy" {
-#  name        = "${upper(var.prefix)}Kinesis"
-#  description = "Policy to allow Kinesis stream operations and CloudWatch logging"
-#  policy      = jsonencode({
-#    Version = "2012-10-17",
-#    Statement = [
-#      # Allow Kinesis actions on the specific stream
-#      {
-#        Action = [
-#          "kinesis:CreateStream",
-#          "kinesis:DescribeStream",
-#          "kinesis:DescribeStreamSummary",
-#          "kinesis:ListStreams",
-#          "kinesis:ListShards",
-#          "kinesis:PutRecord",
-#          "kinesis:PutRecords",
-#          "kinesis:GetRecords",
-#          "kinesis:GetShardIterator"
-#        ],
-#        Effect   = "Allow",
-#        Resource = "arn:aws:kinesis:${var.region}:${var.account_id}:stream/${aws_kinesis_stream.test_stream.name}"
-#      },
-#      # Allow CloudWatch Logs actions on the specific log group
-#      {
-#        Action = [
-#          "logs:PutLogEvents",
-#          "logs:CreateLogStream"
-#        ],
-#        Effect   = "Allow",
-#        Resource = [
-#          aws_cloudwatch_log_group.kinesis_log_group.arn,
-#          "${aws_cloudwatch_log_group.kinesis_log_group.arn}:*"
-#        ]
-#      },
-#      # Allow Lambda execution permissions
-#      {
-#        Action = [
-#          "lambda:InvokeFunction",
-#          "lambda:GetFunctionConfiguration"
-#        ],
-#        Effect   = "Allow",
-#        Resource = aws_lambda_function.kinesis_lambda_function.arn
-#      }
-#    ]
-#  })
-# }
-#
+data "aws_iam_policy_document" "firehose_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "lambda:InvokeFunction",
+      "lambda:GetFunctionConfiguration"
+    ]
+    resources = [
+      aws_lambda_function.amp_publisher.arn
+    ]
+  }
 
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/kinesis_stream
-resource "aws_kinesis_stream" "metrics" {
-  name             = "${var.prefix}-metrics"
-  # shard_count      = var.metrics_stream_shard_count
-  retention_period = 48 # Retain data for 48 hours
-  shard_level_metrics = [
-    "IncomingBytes",
-    "OutgoingBytes"
-  ]
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:PutLogEvents",
+      "logs:CreateLogStream"
+    ]
+    resources = [
+      aws_cloudwatch_log_group.project_logs.arn,
+      "${aws_cloudwatch_log_group.project_logs.arn}:*"
+    ]
+  }
 
-  stream_mode_details {
-    stream_mode = "ON_DEMAND"
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:PutObject",
+      "s3:GetBucketLocation",
+      "s3:ListBucket"
+    ]
+    resources = [
+      aws_s3_bucket.metrics_bucket.arn,
+      "${aws_s3_bucket.metrics_bucket.arn}/*"
+    ]
   }
 }
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_policy
+resource "aws_iam_policy" "firehose_policy" {
+  name        = "${title(var.prefix)}FirehosePolicy"
+  description = "Policy to allow Firehose operations and CloudWatch logging"
+  policy = data.aws_iam_policy_document.firehose_policy.json
+}
+
+# # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/kinesis_stream
+# resource "aws_kinesis_stream" "metrics" {
+#   name             = "${var.prefix}-metrics"
+#   # shard_count      = var.metrics_stream_shard_count
+#   retention_period = 48 # Retain data for 48 hours
+#   shard_level_metrics = [
+#     "IncomingBytes",
+#     "OutgoingBytes"
+#   ]
+#
+#   stream_mode_details {
+#     stream_mode = "ON_DEMAND"
+#   }
+# }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudwatch_log_stream
 resource "aws_cloudwatch_log_stream" "metric_delivery_stream_logs" {
@@ -124,10 +97,10 @@ resource "aws_kinesis_firehose_delivery_stream" "metrics" {
   name        = "${var.prefix}-metrics"
   destination = "extended_s3"
 
-  kinesis_source_configuration {
-    kinesis_stream_arn = aws_kinesis_stream.metrics.arn
-    role_arn           = aws_iam_role.firehose_role.arn
-  }
+  # kinesis_source_configuration {
+  #   kinesis_stream_arn = aws_kinesis_stream.metrics.arn
+  #   role_arn           = aws_iam_role.firehose_role.arn
+  # }
 
   extended_s3_configuration {
     role_arn           = aws_iam_role.firehose_role.arn
@@ -150,7 +123,7 @@ resource "aws_kinesis_firehose_delivery_stream" "metrics" {
 
         parameters {
           parameter_name  = "LambdaArn"
-          parameter_value = "${aws_lambda_function.kinesis_to_amp.arn}:$LATEST"
+          parameter_value = aws_lambda_function.amp_publisher.arn
         }
 
         # Optional: Include retry duration if needed
@@ -171,49 +144,7 @@ resource "aws_kinesis_firehose_delivery_stream" "metrics" {
   #   }
 }
 
-#
-## Kinesis Firehose Delivery Stream
-## resource "aws_kinesis_firehose_delivery_stream" "firehose_stream" {
-##   name        = "cloudwatch-metric-stream"
-##   destination = "extended_s3"
-##   extended_s3_configuration {
-##     role_arn   = "arn:aws:iam::471112885190:role/cloudwatch_metric_stream_role"
-##     bucket_arn = "arn:aws:s3:::adobe-secure-metrics-data"
-##   }
-## }
-## CloudWatch Metric Stream for DynamoDB Metrics
-#resource "aws_cloudwatch_metric_stream" "dynamodb_metric_stream" {
-#  name          = "dynamodb-metric-stream"
-#  role_arn      = "arn:aws:iam::471112885190:role/cloudwatch_metric_stream_role"
-#  firehose_arn  = "arn:aws:firehose:us-east-1:471112885190:deliverystream/KDS-S3-oBr67"
-#  output_format = "json"
-#  # Include specific DynamoDB metrics
-#  include_filter {
-#    namespace = "AWS/DynamoDB"
-#    metric_names = [
-#      "ConditionalCheckFailedRequests",
-#      "ConsumedReadCapacityUnits",
-#      "ConsumedWriteCapacityUnits",
-#      "ReadThrottleEvents",
-#      "ReturnedBytes",
-#      "ReturnedItemCount",
-#      "ReturnedRecordsCount",
-#      "SuccessfulRequestLatency",
-#      "SystemErrors",
-#      "TimeToLiveDeletedItemCount",
-#      "ThrottledRequests",
-#      "UserErrors",
-#      "WriteThrottleEvents",
-#      "OnDemandMaxReadRequestUnits",
-#      "OnDemandMaxWriteRequestUnits",
-#      "AccountMaxReads",
-#      "AccountMaxTableLevelReads",
-#      "AccountMaxTableLevelWrites",
-#      "AccountMaxWrites",
-#      "ThrottledPutRecordCount"
-#    ]
-#  }
-#}
+
 ## Alarm for ThrottledRequests
 #resource "aws_cloudwatch_metric_alarm" "throttled_requests" {
 #alarm_name          = "dynamodb-throttled-requests"
--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -3,110 +3,70 @@
 data "aws_iam_policy_document" "lambda_assume_role" {
   statement {
     effect = "Allow"
+    actions = [
+      "sts:AssumeRole"
+    ]
 
     principals {
       type        = "Service"
       identifiers = ["lambda.amazonaws.com"]
     }
-
-    actions = ["sts:AssumeRole"]
   }
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role
 resource "aws_iam_role" "lambda_role" {
-  name               = "${upper(var.prefix)}LambdaRole"
+  name               = "${title(var.prefix)}CloudMetricsAmpWriterRole"
   assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
 }
 
-# resource "aws_iam_policy" "kinesis_policy" {
-#   name        = "KtaKinesisPolicy"
-#   description = "Policy to allow Kinesis stream operations and CloudWatch logging"
-#   policy = jsonencode({
-#     Version = "2012-10-17",
-#     Statement = [
-#       # Allow Kinesis actions on the specific stream
-#       {
-#         Action = [
-#           "kinesis:CreateStream",
-#           "kinesis:DescribeStream",
-#           "kinesis:DescribeStreamSummary",
-#           "kinesis:ListStreams",
-#           "kinesis:ListShards",
-#           "kinesis:PutRecord",
-#           "kinesis:PutRecords",
-#           "kinesis:GetRecords",
-#           "kinesis:GetShardIterator"
-#         ],
-#         Effect   = "Allow",
-#         Resource = "arn:aws:kinesis:${var.region}:${var.account_id}:stream/${aws_kinesis_stream.metrics_stream.name}"
-#       },
-#       # Allow CloudWatch Logs actions on the specific log group
-#       {
-#         Action = [
-#           "logs:PutLogEvents",
-#           "logs:CreateLogStream"
-#         ],
-#         Effect = "Allow",
-#         Resource = [
-#           aws_cloudwatch_log_group.kinesis_log_group.arn,
-#           "${aws_cloudwatch_log_group.kinesis_log_group.arn}:*"
-#         ]
-#       },
-#       # Allow Lambda execution permissions
-#       {
-#         Action = [
-#           "lambda:InvokeFunction",
-#           "lambda:GetFunctionConfiguration"
-#         ],
-#         Effect   = "Allow",
-#         Resource = aws_lambda_function.kinesis_to_amp.arn
-#       }
-#     ]
-#   })
-# }
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/iam_policy_document
+data "aws_iam_policy_document" "lambda_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "aps:RemoteWrite"
+    ]
+    resources = [
+      aws_prometheus_workspace.amp_workspace.arn
+    ]
+  }
+}
 
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_policy
+resource "aws_iam_policy" "lambda_write" {
+  name        = "${upper(var.prefix)}LambdaPolicy"
+  description = "Policy to allow Kinesis stream operations and CloudWatch logging"
+  policy      = data.aws_iam_policy_document.lambda_policy.json
+}
 
-# resource "aws_iam_policy" "amp_policy" {
-#   name        = "KtaAmpPolicy"
-#   description = "Allows operations on Amazon Managed Prometheus"
-#
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [{
-#       Effect = "Allow"
-#       Action = [
-#         "aps:*"
-#       ]
-#       Resource = "*"
-#     }]
-#   })
-# }
-#
-# resource "aws_iam_role_policy_attachment" "amp_attachment" {
-#   role       = aws_iam_role.lambda_role.id
-#   policy_arn = aws_iam_policy.amp_policy.arn
-# }
-#
-# resource "aws_iam_role_policy_attachment" "kinesis_attachment" {
-#   role       = aws_iam_role.lambda_role.id
-#   policy_arn = aws_iam_policy.kinesis_policy.arn
-# }
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/iam_policy
+data "aws_iam_policy" "lambda_execution" {
+  name = "AWSLambdaBasicExecutionRole"
+}
 
-# # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudwatch_log_stream
-# resource "aws_cloudwatch_log_stream" "kinesis_log_stream" {
-#  name           = "test-kinesis-log-stream"
-#  log_group_name = aws_cloudwatch_log_group.project_logs.name
-# }
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy_attachment
+resource "aws_iam_role_policy_attachment" "lambda_attachment" {
+  role       = aws_iam_role.lambda_role.id
+  policy_arn = data.aws_iam_policy.lambda_execution.arn
+}
 
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy_attachment
+resource "aws_iam_role_policy_attachment" "lambda_attachment_2" {
+  role       = aws_iam_role.lambda_role.id
+  policy_arn = aws_iam_policy.lambda_write.arn
+}
 
-resource "aws_lambda_function" "kinesis_to_amp" {
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/lambda_function
+resource "aws_lambda_function" "amp_publisher" {
   function_name    = "${var.prefix}-amp-publisher"
   runtime          = "java11"
   handler          = "com.adobe.aep.metrics.CloudWatchMetricsAmpWriter::handleRequest"
   role             = aws_iam_role.lambda_role.arn
   filename         = var.lambda_writer_config.jar_file
   source_code_hash = filebase64sha256(var.lambda_writer_config.jar_file)
+
+
 
   environment {
     variables = {
@@ -124,106 +84,13 @@ resource "aws_lambda_function" "kinesis_to_amp" {
   timeout     = var.lambda_writer_config.timeout
 }
 
+resource "aws_lambda_permission" "allow_firehose" {
+  statement_id  = "AllowFirehoseInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.amp_publisher.function_name
+  principal     = "firehose.amazonaws.com"
 
-# Kinesis Stream and Lambda Event Source Mapping
-# resource "aws_lambda_event_source_mapping" "kinesis_to_amp" {
-#   event_source_arn = aws_kinesis_stream.test_stream.arn
-#   function_name    = aws_lambda_function.kinesis_to_amp.arn
-#   starting_position = "LATEST"
-# }
+  # Optionally, you can specify the source ARN if you want to restrict it to a specific Firehose stream
+  source_arn = aws_kinesis_firehose_delivery_stream.metrics.arn
+}
 
-
-
-# resource "aws_lambda_function" "kinesis_stream_processor" {
-#  function_name = "kinesis-stream-processor"
-#  role          = aws_iam_role.lambda_execution_role.arn
-#
-#  # Runtime and handler configuration
-#  runtime = "java11"
-#  handler = "com.example.LambdaHandler::handleRequest"
-#
-#  # Resource configurations
-#  memory_size = 512
-#  timeout     = 300
-#
-#  # Storage and package configuration
-#  package_type = "Zip"
-#  ephemeral_storage {
-#    size = 512
-#  }
-#
-#  # Logging configuration
-#  logging_config {
-#    log_format = "Text"
-#    log_group  = "/aws/lambda/kinesis-stream-processor"
-#  }
-#
-#  # Tracing configuration
-#  tracing_config {
-#    mode = "PassThrough"
-#  }
-#
-#  # Environment variables
-#  environment {
-#    variables = {
-#      AWS_AMP_ROLE_ARN            = aws_iam_role.lambda_execution_role.arn
-#      DEFAULT_AWS_REGION          = "us-east-1"
-#      PROMETHEUS_REMOTE_WRITE_URL = "https://aps-workspaces.us-east-1.amazonaws.com/workspaces/ws-14e20584-7c40-48f5-a8f4-fd3e772d82d4/api/v1/remote_write" // Change to your actually endpoint.
-#    }
-#  }
-#
-#  # Architecture specification
-#  architectures = ["x86_64"]
-#
-#  # Tags
-#  tags = {
-#    Environment = "Dev"
-#  }
-#}
-#
-## IAM Role Resource (inferred from the role ARN)
-#resource "aws_iam_role" "lambda_execution_role" {
-#  name = "lambda_execution_role"
-#
-#  assume_role_policy = jsonencode({
-#    Version = "2012-10-17"
-#    Statement = [
-#      {
-#        Action = "sts:AssumeRole"
-#        Effect = "Allow"
-#        Principal = {
-#          Service = "lambda.amazonaws.com"
-#        }
-#      }
-#    ]
-#  })
-#}
-
-
-## IAM Role for Lambda Execution
-## data "aws_iam_role" "lambda_execution_role" {
-##   name               = "lambda_execution_role"
-##   assume_role_policy = jsonencode({
-##     Version = "2012-10-17",
-##     Statement = [
-##       {
-##         Action = "sts:AssumeRole",
-##         Effect = "Allow",
-##         Principal = {
-##           Service = "lambda.amazonaws.com"
-##         }
-##       }
-##     ]
-##   })
-## }
-## IAM Policy Attachment for Lambda Role
-#resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
-#  role       = "arn:aws:iam::471112885190:role/lambda_execution_roleS"
-#  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-#}
-## Kinesis Stream and Lambda Event Source Mapping
-#resource "aws_lambda_event_source_mapping" "kinesis_to_lambda" {
-#  event_source_arn = aws_kinesis_stream.test_stream.arn
-#  function_name    = aws_lambda_function.kinesis_lambda_function.arn
-#  starting_position = "LATEST"
-#}
